4bc5d108ec0151b8d527a299d93c4568cabaf50a
mdg1317 <matthew.dennis137@gmail.com>
2022-08-12 12:46:15 -0700

    Data from table now loads in HTML. Uses separate scripts for each page

--- public/javascripts/accessData_Individual.js
+++ public/javascripts/accessData_Individual.js
@@ -16,9 +16,13 @@ const loadTable = async () => {
 };
 
 function loadPage(data){
-	document.getElementById("result0").innerHTML = data[0].number + " - " + data[0].driver;
-	document.getElementById("result1").innerHTML = data[1].number + " - " + data[1].driver;
-	document.getElementById("result2").innerHTML = data[2].number + " - " + data[2].driver;
+	document.getElementById("header").innerHTML = data[0].number + " - " + data[0].driver;
+	document.getElementById("driver").innerHTML = data[0].driver;
+	document.getElementById("number").innerHTML = data[0].number;
+	document.getElementById("series").innerHTML = data[0].series;
+	document.getElementById("sponsor").innerHTML = data[0].sponsor;
+	document.getElementById("manufacturer").innerHTML = data[0].manufacturer;
+	document.getElementById("year").innerHTML = data[0].year;
 }
 
 /*function storeTable(data){
