d04ecb7a601a40bab9ac14521c56899477aca173
mdg1317 <matthew.dennis137@gmail.com>
2022-08-12 12:53:01 -0700

    Same script is now used for each page

--- public/javascripts/accessData.js
+++ public/javascripts/accessData.js
@@ -1,5 +1,3 @@
-let table = [];
-
 const loadTable = async () => {
 	console.log("Attempting to access table...");
 	$.post({
@@ -7,7 +5,6 @@ const loadTable = async () => {
 		success: function(data, ) {
 			console.log("Successfully accessed table");
 			console.log(data);
-			//storeTable(data);
 			loadPage(data);
 		}
 	}).fail(function(jqxhr, settings, ex) {
@@ -16,40 +13,21 @@ const loadTable = async () => {
 };
 
 function loadPage(data){
-	document.getElementById("result0").innerHTML = data[0].number + " - " + data[0].driver;
-	document.getElementById("result1").innerHTML = data[1].number + " - " + data[1].driver;
-	document.getElementById("result2").innerHTML = data[2].number + " - " + data[2].driver;
+	// NOT THE BEST WAY TO DO THIS
+	// TRY TO IMPROVE AT SOME POINT
+	if(window.location.href.indexOf("individual_page") > -1) {
+		document.getElementById("header").innerHTML = data[0].number + " - " + data[0].driver;
+		document.getElementById("driver").innerHTML = data[0].driver;
+		document.getElementById("number").innerHTML = data[0].number;
+		document.getElementById("series").innerHTML = data[0].series;
+		document.getElementById("sponsor").innerHTML = data[0].sponsor;
+		document.getElementById("manufacturer").innerHTML = data[0].manufacturer;
+		document.getElementById("year").innerHTML = data[0].year;
+	} else {
+		document.getElementById("result0").innerHTML = data[0].number + " - " + data[0].driver;
+		document.getElementById("result1").innerHTML = data[1].number + " - " + data[1].driver;
+		document.getElementById("result2").innerHTML = data[2].number + " - " + data[2].driver;
+	}
 }
 
-/*function storeTable(data){
-	//table = data;
-	console.log(data[0].id);
-	var i;
-	for(i = 0; i < data.length; i++){
-		var myId = data[i].id;
-		var myDriver = data[i].driver;
-		var myNumber = data[i].number;
-		var mySeries = data[i].series;
-		var mySponsor = data[i].sponsor;
-		var myManufacturer = data[i].manufacturer;
-		var myYear = data[i].year;
-		var myImage = data[i].image;
-		var carJSON = {
-			id: myId,
-			driver: myDriver,
-			number: myNumber,
-			series: mySeries,
-			sponsor: mySponsor,
-			manufacturer: myManufacturer,
-			year: myYear,
-			image: myImage
-		}
-		table[i] = carJSON;
-	}
-}*/
-
-$(document).ready(function() {
-	loadTable();
-	//console.log(table);
-	//document.getElementById("result0").innerHTML = table;
-});
+loadTable();
