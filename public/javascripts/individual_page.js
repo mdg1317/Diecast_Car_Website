a560da647a9fb82f49b744231bfc775a7bbcf569
mdg1317 <matthew.dennis137@gmail.com>
2022-09-15 19:11:19 -0700

    Added simple previous, next, and back buttons

--- public/javascripts/individual_page.js
+++ public/javascripts/individual_page.js
@@ -22,8 +22,38 @@ function fillPage(data){
 }
 
 $(document).ready(function() {
-	var carArray = JSON.parse(sessionStorage.getItem("tableData"));
+	var carArray;
+
+	// If a search was made, use that subset
+	// If not, use whole array
+	if(sessionStorage.getItem("searchData") != null){
+		console.log("searchData exists");
+		carArray = JSON.parse(sessionStorage.getItem("searchData"));
+	} else {
+		console.log("searchData does not exist");
+		carArray = JSON.parse(sessionStorage.getItem("tableData"));
+	}
+
+	// Get specific entry that matches id
 	var carData = carArray.find(o => o.id == location.search.substring(1));
 	console.log(carData);
+	console.log(carArray.indexOf(carData));
 	fillPage(carData);
+
+	var prevButton = document.getElementById("prevButton");
+	var nextButton = document.getElementById("nextButton");
+
+	// If at first or last entry, hide respective button
+	var currentIndex = carArray.indexOf(carData);
+	if(currentIndex + 1 >= carArray.length){
+		nextButton.setAttribute("hidden", true);
+	} else {
+		nextButton.href = "individual_page.html?" + carArray[currentIndex + 1].id;
+	}
+
+	if(currentIndex - 1 < 0){
+		prevButton.setAttribute("hidden", true);
+	} else {
+		prevButton.href = "individual_page.html?" + carArray[currentIndex - 1].id;
+	}
 });
